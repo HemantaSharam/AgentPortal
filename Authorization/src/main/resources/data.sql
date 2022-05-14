3b27422d272fce8042b6d596151ead2cdb22c6ff
Hemanta Sharma <hmnt.sharma092@gmail.com>
2022-05-14 22:31:48 +0530

    sql update

--- Authorization/src/main/resources/data.sql
+++ Authorization/src/main/resources/data.sql
@@ -1,3 +1,3 @@
 insert into agent (agentid,password) values ('hemanta','hemanta123');
 insert into agent (agentid,password) values ('ravi','ravi123');
-insert into agent (agentid,password) values ('shivam','shivam13');
+insert into agent (agentid,password) values ('shivam','shivam132');
